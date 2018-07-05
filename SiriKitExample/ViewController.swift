9b302f44ceef29596d6ba4638aa9f486d41563c5
Carlos Irano <cairano@gmail.com>
2018-07-05 03:57:24 -0300

    First commit

---
+++ SiriKitExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SiriKitExample
+//
+//  Created by Carlos Irano on 05/07/18.
+//  Copyright © 2018 Carlos Irano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
