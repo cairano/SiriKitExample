ce9c83942d7bfccebe699500b23bdb3a176e4fda
Carlos Irano <cairano@gmail.com>
2018-07-05 05:09:45 -0300

    Creating an Intents App Extension

--- SiriKitExample/ViewController.swift
+++ SiriKitExample/ViewController.swift
@@ -7,18 +7,26 @@
 //
 
 import UIKit
+import Intents
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // request Siri Authorization
+        INPreferences.requestSiriAuthorization { (status) in
+            print("status /status")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
